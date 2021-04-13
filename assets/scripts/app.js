bdacc5379821dedcd317f29c6d63eb4081a3c8a9
NicoooM <nmarsan33000@gmail.com>
2021-04-13 16:48:33 +0200

    add logo + logo interaction

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,13 @@
 const travel = document.querySelector("#travel");
 const landing = document.querySelector(".landing");
+const logo = document.querySelector("#logo");
 
 travel.addEventListener("click", () => {
-  landing.classList.toggle("closed");
+  landing.classList.add("closed");
   console.log("clicked");
+  if (landing.classList.contains("closed")) {
+    logo.addEventListener("click", () => {
+      landing.classList.remove("closed");
+    });
+  }
 });
